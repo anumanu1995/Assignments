8c521a36bf5f7c96d9760f5d21904722bc91b666
rthoka-rpx <33126558+rthoka-rpx@users.noreply.github.com>
2022-07-26 19:07:16 +0530

    Update Student.rb

--- student/Student.rb
+++ student/Student.rb
@@ -29,3 +29,15 @@ end
 
 stud = StudentDemo.new
 stud.getStudDetails(10,'Anu',10,15,20)
+
+#please do follow Ruby naming conventions for creating method names like_this
+#for calculating Student marks, please think of writing code with minimal changes for the future changes
+#say for example what if the User wants add an extra subject and it's marks
+#with the current approach, we need to change code at reading input/ initializing/ calculating total , in almost all stages
+#you can use something likes this : [1,2,3].inject(0){|sum, marks| sum = sum + marks} instead of summing all of them individually
+#and there is an *args param, think of it how we can use that in this scenario.
+
+
+
+#The code too tightly coupled, not a recomanded approach.
+
